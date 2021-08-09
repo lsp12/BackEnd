e6b5e2b08f8183af703e5879987f5918116841c1
CrisLDev <leonardomoreirapazmio@gmail.com>
2021-08-08 23:09:31 -0500

    Added(Aditional configuration for eslint)

--- src/dataBase/database.ts
+++ src/dataBase/database.ts
@@ -1,17 +1,16 @@
-import mongoose, { ConnectOptions } from "mongoose";
-import { MONGO_URI } from "./Url";
+import mongoose, { ConnectOptions } from 'mongoose';
+import { MONGO_URI } from './Url';
 
-export const socialNetworkAPI = async ()=>{
-    try {
-        const mongoOption: ConnectOptions = {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useFindAndModify: false,
-        }
-
-        const db = await mongoose.connect(MONGO_URI, mongoOption);
-        console.log("Connected to database", db.connection.name);
-    } catch (error) {
-        console.log("Error connecting to database",error);
-    }
-}
+export const socialNetworkAPI = async () => {
+  try {
+    const mongoOption: ConnectOptions = {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    };
+    const db = await mongoose.connect(MONGO_URI, mongoOption);
+    console.log('Connected to database', db.connection.name);
+  } catch (error) {
+    console.log('Error connecting to database', error);
+  }
+};
