2da71d00076d4fe7da94c1c94778a6151da9aaf6
lsp12 <jonathankenny852@gmail.com>
2021-08-15 20:03:38 -0500

    change

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import cors from 'cors';
-import express from 'express'
+import express from 'express';
 import morgan from 'morgan';
-import {route} from './routes/index.routes'
-import {socialNetworkAPI} from './dataBase/database'
-socialNetworkAPI();
-const app = express()
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+import { route } from './routes/index.routes';
+import { dataBase } from './dataBase/database';
+dataBase();
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/network', route.userAuth);
 export default app;
