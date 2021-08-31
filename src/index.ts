0d34dc5183f57730b557e6c3130c4c44fec0b1f6
CrisLDev <leonardomoreirapazmio@gmail.com>
2021-08-30 20:54:49 -0500

    Attemtin automatic deploy

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import app from './app';
 import { dataBase } from './database/database';
 dataBase();
 app.listen(app.get('port'), () => {
-  console.log('server on port', app.get('port'));
+  console.log('Server on port', app.get('port'));
 });
