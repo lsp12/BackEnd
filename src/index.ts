7043f4f3e34998e949660dec2a2a86d3167733d6
lsp12 <jonathankenny852@gmail.com>
2021-10-01 20:17:35 -0500

    heroku

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import app from './app';
 import './database';
-import { Server as WebSocket } from 'socket.io';
-import https from 'http';
-import { chatSocket } from './socket/chat/chatSocket';
 import initSocket from './socket/initSocket';
 
 const server = initSocket(app);
